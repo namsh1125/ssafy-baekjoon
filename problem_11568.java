a349b64252db75e48656cefda74cda116e6d4781
SeungHyeon <namsh1125@naver.com>
2025-10-29 19:20:00 +0900

    Solved at 2025-10-29

---
+++ problem_11568.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        int max = 1;
+        for (int i = 1; i < N; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
+        }
+
+        System.out.println(max);
+        br.close();
+    }
+
+}
