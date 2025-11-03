820cf58950b31db656ef092ea00f0e846940a0b6
SeungHyeon <namsh1125@naver.com>
2025-11-03 21:33:24 +0900

    Solved at 2025-11-03

---
+++ problem_15990.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static final int MAX = 100_000;
+    private static final int MOD = 1_000_000_009;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[MAX + 1][4]; // dp[i][j]: i인 수를 만들기 위해 마지막으로 사용한 수가 j
+        dp[1][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
+
+        for (int i = 4; i <= MAX; i++) {
+            dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % MOD;
+            dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % MOD;
+            dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % MOD;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            long result = (dp[n][1] + dp[n][2] + dp[n][3]) % MOD;
+            sb.append(result).append("\n");
+        }
+
+        System.out.print(sb.toString().trim());
+        br.close();
+    }
+
+}
